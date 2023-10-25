042ec9b25889f69999b50648b491f7d99ce19f0d
BogdanModolea <bogdan.modolea@stud.ubbcluj.ro>
2023-10-25 14:28:48 +0300

    Tests [2]

--- Lab03/src/main/java/org/example/Main.java
+++ Lab03/src/main/java/org/example/Main.java
@@ -2,29 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        SymbolTable symbolTable = new SymbolTable(8);
-
-        String[] symbols = {"x", "y", "t", "i", "y"};
-
-        System.out.println("Add --------------");
-        for (String s : symbols) {
-            System.out.println(symbolTable.addSymbol(s));
-        }
-
-        System.out.println("\nContains --------------");
-        for (String symbol : symbols) {
-            System.out.println(symbolTable.containsSymbol(symbol));
-        }
-
-        symbols = new String[]{"i", "x", "t", "y", "z"};
-        System.out.println("\nPosition --------------");
-        for (String symbol : symbols) {
-            System.out.println(symbolTable.getPosition(symbol));
-        }
-
-        System.out.println("\nRemove --------------");
-        for (String symbol : symbols) {
-            System.out.println(symbolTable.removeSymbol(symbol));
-        }
+        System.out.println("Hello world!");
     }
 }
