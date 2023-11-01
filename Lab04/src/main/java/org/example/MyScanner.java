8905765915199e5683bdb5e8112eebcd5d3d0dec
BogdanModolea <bogdan.modolea@stud.ubbcluj.ro>
2023-11-01 14:33:51 +0200

    Scanner [2]

--- Lab04/src/main/java/org/example/MyScanner.java
+++ Lab04/src/main/java/org/example/MyScanner.java
@@ -15,20 +15,17 @@ public class MyScanner {
     private final String CONST_REGEX = "^0|[1-9]([0-9])*|[-|+][1-9]([0-9])*|'[1-9]'|'[a-zA-Z]'|\"[0-9]*[a-zA-Z ]*\"$";
     private final String IDENTIFIER_REGEX = "^[A-Za-z][A-Za-z0-9]*";
 
-//    private final ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/", "<=", ">=", "==", "<", ">", "=", "++", "%", "+="));
     private final ArrayList<String> operators = new ArrayList<>();
-//    private final ArrayList<String> separators = new ArrayList<>(Arrays.asList("{", "}", "(", ")", ";", " ", ",", "\t", "\n", "'", "\""));
     private final ArrayList<String> separators = new ArrayList<>();
-//    private final ArrayList<String> reservedWords = new ArrayList<>(Arrays.asList("letInt", "letString", "letBool", "letDouble", "if", "else", "print", "read", "for"));
     private final ArrayList<String> reservedWords = new ArrayList<>();
 
     private final String filePath;
     private SymbolTable st;
-    private List<Pair<String, Pair<Integer, Integer>>> pif;
+    private List<Pair<String, String>> pif;
 
     public MyScanner(String filePath) throws FileNotFoundException {
         this.filePath = filePath;
-        this.st = new SymbolTable(101);
+        this.st = new SymbolTable(10);
         this.pif = new ArrayList<>();
         readTokens();
     }
@@ -85,35 +82,30 @@ public class MyScanner {
 
     /**
      * Splits the source code into a list of tokens using separators.
-     * @return the list of tokens.
+     * Processes the list of tokens and categorizes them.
+     * @return a list of pairs (token, line) where each token is associated with the line it appears on.
      */
-    private List<String> splitProgram() {
+    List<Pair<String, Integer>> getTokens() {
+        List<String> tokens = new ArrayList<>();
+        List<Pair<String, Integer>> ans = new ArrayList<>();
+        int line = 1;
+
         try {
             String code = readProgram();
-            String separatorsString = this.separators.stream()
-                    .reduce("", (s, t) -> (s + t));
-            StringTokenizer tokenizer = new StringTokenizer(code, separatorsString, true);
+            StringBuilder separatorsString = new StringBuilder();
 
-            List<String> tokens = new ArrayList<>();
+            for(String separator : separators) {
+                separatorsString.append(separator);
+            }
+
+            StringTokenizer tokenizer = new StringTokenizer(code, separatorsString.toString(), true);
+
             while (tokenizer.hasMoreTokens()) {
                 tokens.add(tokenizer.nextToken());
             }
-            return tokens;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        return null;
-    }
-
-    /**
-     * Processes a list of tokens and categorizes them.
-     * @param tokens
-     * @return a list of pairs (token, line) where each token is associated with the line it appears on.
-     */
-    List<Pair<String, Integer>> getTokens(List<String> tokens) {
-        List<Pair<String, Integer>> ans = new ArrayList<>();
-        int line = 1;
 
         for (int i = 0; i < tokens.size(); i++) {
             if(!Objects.equals(tokens.get(i), " ")) {
@@ -148,38 +140,30 @@ public class MyScanner {
      * Any lexical errors are reported. The results are stored in the PIF and symbol table.
      */
     public void scan() {
-        List<String> splitTokens = splitProgram();
-        boolean lexicalErrorExists = false;
+        boolean error = false;
 
-        if (splitTokens == null) {
-            return;
-        }
-
-        List<Pair<String, Integer>> tokens = getTokens(splitTokens);
+        List<Pair<String, Integer>> tokens = getTokens();
         for (Pair<String, Integer> t : tokens) {
             String token = t.getFirst();
 
-
             if (this.reservedWords.contains(token)) {
-                this.pif.add(new Pair<>(token, new Pair<>(-1, -1)));
+                this.pif.add(new Pair<>(token, "-1"));
             } else if (this.operators.contains(token)) {
-                this.pif.add(new Pair<>(token, new Pair<>(-1, -1)));
+                this.pif.add(new Pair<>(token, "-1"));
             } else if (this.separators.contains(token)) {
-                this.pif.add(new Pair<>(token, new Pair<>(-1, -1)));
+                this.pif.add(new Pair<>(token, "-1"));
             } else if (Pattern.compile(CONST_REGEX).matcher(token).matches()) {
                 this.st.addSymbol(token);
-                this.pif.add(new Pair<>("CONST", st.getPosition(token)));
+                this.pif.add(new Pair<>("CONST", st.getPosition(token).getFirst().toString()));
+//                this.pif.add(new Pair<>("CONST", token));
             } else if (Pattern.compile(IDENTIFIER_REGEX).matcher(token).matches()) {
                 this.st.addSymbol(token);
-                this.pif.add(new Pair<>("ID", st.getPosition(token)));
+                this.pif.add(new Pair<>("ID", st.getPosition(token).getFirst().toString()));
+//                this.pif.add(new Pair<>("ID", token));
             } else {
                 System.out.println("lexical error at line " + t.getSecond() + ": " + t.getFirst());
-                lexicalErrorExists = true;
+                error = true;
             }
-        }
-
-        if (!lexicalErrorExists) {
-            System.out.println("Program is lexically correct!");
         }
     }
 
@@ -187,9 +171,21 @@ public class MyScanner {
     public String getPif(){
         StringBuilder computedString = new StringBuilder();
         for(int i = 0; i < this.pif.size(); i++) {
-            computedString.append(this.pif.get(i).getFirst())
-                    .append(" - ")
+            int len1 = this.pif.get(i).getFirst().length();
+            computedString
+                    .append("| ")
+                    .append(this.pif.get(i).getFirst());
+            for(int j = 0; j < 10 - len1; j++) {
+                computedString.append(" ");
+            }
+            computedString
+                    .append("| ");
+            if(!Objects.equals(this.pif.get(i).getSecond(), "-1")) {
+                computedString.append(" ");
+            }
+            computedString
                     .append(this.pif.get(i).getSecond())
+                    .append(" |")
                     .append("\n");
         }
 
